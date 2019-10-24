020540dd6ed2905246db7534f82b7cf8a9346566
mrmandarin2002 <46353341+mrmandarin2002@users.noreply.github.com>
2019-10-23 22:13:14 -0500

    Work in progress!

--- CLIENT/src/sample/Main.java
+++ CLIENT/src/sample/Main.java
@@ -2,8 +2,15 @@ package sample;
 
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
+import javafx.geometry.Pos;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.layout.BorderPane;
+import javafx.scene.paint.Color;
+import javafx.scene.text.Font;
+import javafx.scene.text.FontWeight;
+import javafx.scene.text.Text;
+import javafx.scene.text.TextAlignment;
 import javafx.stage.Stage;
 import javafx.event.ActionEvent;
 import javafx.event.EventHandler;
@@ -18,9 +25,14 @@ public class Main extends Application {
     Stage client_window;
 
     //scenes & layouts
-    Scene welcome_screen, menu_screen;
-    VBox welcome_layout;
-    StackPane menu_layout;
+    Scene welcome_screen, menu_screen, textbook_distribution_screen;
+    BorderPane welcome_layout = new BorderPane(), menu_layout = new BorderPane();
+    VBox welcome_center = new VBox(), menu_center = new VBox();
+
+    //Display values
+    String display_font = "Times New Roman";
+    int resolution_y = 500;
+    int resolution_x = 650;
 
     public static void main(String[] args){
         launch(args);
@@ -29,31 +41,58 @@ public class Main extends Application {
     @Override
     public void start(Stage primaryStage) throws Exception{
         client_window = primaryStage;
+        client_window.setTitle("DigiText");
+
         Parent root = FXMLLoader.load(getClass().getResource("sample.fxml"));
 
-        //welcome screen
-        Label welcome_text = new Label("Welcome");
-        Button click_to_continue = new Button("CLICK THIS TO CONTINUE");
-        click_to_continue.setOnAction(e -> client_window.setScene(menu_screen));
-        welcome_layout = new VBox(20);
-        welcome_layout.getChildren().addAll(welcome_text, click_to_continue);
 
-        welcome_screen = new Scene(welcome_layout, 500, 500);
+        /* Welcome Screen */
+        //Welcome Label
+        Label welcome_label = new Label("Welcome to DigiText! Press any key or the button to continue");
+        welcome_label.setFont(Font.font(display_font, 20) );
+        welcome_label.setTextFill(Color.color(0.69,0.19,0.38));
 
-        // menu //
-        //menu buttons
-        Button textbook_distribution = new Button("Textbook distribution");
-        Button textbook_return = new Button("Textbook Return");
-        Button instructions = new Button("Instructions");
+        //Welcome Button
+        Button welcome_button = new Button("Press to continue");
+        welcome_button.setOnAction(e-> client_window.setScene(menu_screen));
 
-        //menu layout
-        menu_layout = new StackPane();
-        menu_layout.getChildren().addAll(textbook_distribution, textbook_return, instructions);
-        menu_screen = new Scene(menu_layout, 500, 500);
+        //Welcome Layout
+        welcome_center.getChildren().addAll(welcome_label, welcome_button);
+        welcome_center.setAlignment(Pos.CENTER);
+        welcome_center.setSpacing(10);
+        welcome_layout.setCenter(welcome_center);
+        welcome_screen = new Scene(welcome_layout, resolution_x, resolution_y);
+
+        /* Menu Screen */
+        /*Menu Buttons*/
+        Button textbook_dis_button = new Button("Textbook Distribution");
+        textbook_dis_button.setFont(Font.font(display_font, 15));
+        textbook_dis_button.setOnAction(e-> client_window.setScene(textbook_distribution_screen));
+
+        Button textbook_ret_button = new Button("Textbook Return");
+        textbook_ret_button.setFont(Font.font(display_font, 15));
+
+        Button barcode_button = new Button("Barcode Maker / Textbook Scanner");
+        barcode_button.setFont(Font.font(display_font, 15));
+
+        Button help_button = new Button("hElP?!?");
+        help_button.setFont(Font.font(display_font, 15));
+
+        Button game_button = new Button("Bored?");
+        game_button.setFont(Font.font(display_font, 5));
+
+
+        menu_center.getChildren().addAll(textbook_dis_button, textbook_ret_button, barcode_button, help_button, game_button);
+        menu_center.setAlignment(Pos.CENTER);
+        menu_center.setSpacing(10);
+
+        menu_layout.setCenter(menu_center);
+        menu_screen = new Scene(menu_layout, resolution_x, resolution_y);
+
 
         client_window.setScene(welcome_screen);
-        client_window.setTitle("SPHS Textbook Distribution");
         client_window.show();
+
     }
 
 }
