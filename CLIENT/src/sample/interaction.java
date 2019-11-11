9046a50b1b35b31d935297e55e0e19fde14479e3
heinrichharms-stpauls <heinrichharms@gmail.com>
2019-11-10 19:35:41 -0600

    minor modifications to java networking code

--- CLIENT/src/sample/interaction.java
+++ CLIENT/src/sample/interaction.java
@@ -1,16 +1,32 @@
+package sample;
+
+import java.net.DatagramSocket;
+import java.net.InetAddress;
+import java.net.SocketException;
+import java.net.UnknownHostException;
+
 public class interaction {
 
     // initialize client socket
     private DatagramSocket socket;
     private InetAddress address;
 
-    public interaction() {
+    private String server_address;
+    private int server_port;
+
+    private byte[] buf;
+
+    public interaction(String server_a, int server_p) throws SocketException, UnknownHostException {
+        server_address = server_a;
+        server_port = server_p;
         socket = new DatagramSocket();
         address = InetAddress.getByName("localhost");
     }
     
-    public static String command(String cmd) {
+    public String command(String cmd) {
 
+
+        return "HELLO";
     }
 
     public void close() {
